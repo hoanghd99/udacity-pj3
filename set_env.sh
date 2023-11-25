b120d6a7cbf7164bee0fbae18ae38064e1c0a159
hoanghd99 <hoang.hd.set@gmail.com>
2023-11-25 14:55:26 +0700

    add project

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=hoanghd3
+export POSTGRES_PASSWORD=hoang123
+export POSTGRES_HOST=project3.cjuldzdofxcl.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=project3rds
+export AWS_BUCKET=project3-2990-2779-0702-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+
