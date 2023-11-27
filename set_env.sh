fee6959285af979e23063a5cdbb350d8cb6ba2d8
hoanghd99 <hoang.hd.set@gmail.com>
2023-11-27 20:31:06 +0700

    add config

--- set_env.sh
+++ set_env.sh
@@ -6,7 +6,7 @@ export POSTGRES_HOST=project3.cjuldzdofxcl.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=project3rds
 export AWS_BUCKET=project3-2990-2779-0702-dev
 export AWS_REGION=us-east-1
-export AWS_PROFILE=userproject3
+export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
 
