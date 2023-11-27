ab87b36a7aa4c6757bfe90ea1291f8afd936395f
hoanghd99 <hoang.hd.set@gmail.com>
2023-11-27 23:12:37 +0700

    update screenshot

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a01defec9982347448d56a956d76b0fc-1525770004.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
